b8b3b13b230c023a995afd22e3acc11cd552b67d
Branislav Bilsky <branislav.bilsky@gmail.com>
2013-08-22 17:14:19 +0200

    Create static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+
+    let(:heading)    { 'Site Builder' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
+  end
+
+
+end
