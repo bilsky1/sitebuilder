1a0bebe30de4c12f8850cbf320189c7bf104449e
Branislav Bilsky <branislav.bilsky@gmail.com>
2013-08-27 23:54:48 +0200

    send email on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,20 @@ Sitebuilder::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #send email from localhost via gmail
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "mail.google.com",
+      :user_name => "branislav.bilsky@gmail.com",
+      :password => "0335521213",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
